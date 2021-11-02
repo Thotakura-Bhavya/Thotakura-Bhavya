2b4f2fc438ee1a3696046f130c81798ddcf78b3b
BhavyaThotakura <bhavya.thotakura@ltts.com>
2021-11-02 18:47:22 +0530

    done

---
+++ Activity1/Program.cs
@@ -0,0 +1,39 @@
+﻿using BAL;
+using DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Activity1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Connection String : 
+            //Server Name
+            //Authentication Mode
+            //Name of the Database
+
+            try
+            {
+                HWBL blObj = new HWBL();
+                List<ProductDTO> lstFinalResult = blObj.GetAllDepartments();
+                foreach (var dept in lstFinalResult)
+                {
+                    Console.WriteLine(dept.ProductName );
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Something went wrong. Our developers are working on it.Please quote the error message before when you contact customer care");
+                Console.WriteLine(ex.Message);
+            }
+
+
+
+        }
+    }
+}
